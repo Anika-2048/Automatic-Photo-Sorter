624f230e70d9680cbfd25150baa94546c515d6f4
Anika <128902230+Anika-2048@users.noreply.github.com>
2025-11-27 23:59:11 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -25,4 +25,4 @@ app.include_router(face_recog.router, prefix="", tags=["face_recognition"])
 
 @app.get("/")
 def read_root():
-    return {"status": "API is running", "version": "1.0"}
+    return {"status": "App is running (Catch it)"}
